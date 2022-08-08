c1ba628a4721484f0d65183cc4f6401b004f527d
sarr <sarrabdoulaye93@gmail.com>
2022-08-08 10:28:22 +0000

    fix division 0

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -175,9 +175,24 @@
                     </div>
                     <div class="card-body">
                         <p class="card-text">
-                            Amadou I Sarr : <strong style="color:red; ">  {{ $countAmadou }}</strong> voix ......soit <span style="color:green; font-size:1.3em ">{{round($countAmadou  * 100 / $total,2)}} % </span><br><br>
-                            Alsane Sarr : <strong style="color:red; ">{{ $countAlsane }}</strong> voix ......soit <span style="color:green; font-size:1.3em ">{{round($countAlsane  * 100 / $total,2)}} %</span><br><br>
-                            Bulletin Null : <strong style="color:red;"> {{ $null }}</strong> voix .....soit <span style="color:green; font-size:1.3em "> {{round($null  * 100 / $total,2)}} %</span>
+                            Amadou I Sarr : <strong style="color:red; ">  {{ $countAmadou }}</strong> voix ......soit <span style="color:green; font-size:1.3em ">
+                            @if ($total >0)
+                              {{round($countAmadou  * 100 / $total,2)}} %
+                            @else 0 %
+                            @endif
+                            </span><br><br>
+                            Alsane Sarr : <strong style="color:red; ">{{ $countAlsane }}</strong> voix ......soit <span style="color:green; font-size:1.3em ">
+                                @if ($total >0)
+                                {{round($countAlsane  * 100 / $total,2)}} %
+                              @else 0 %
+                              @endif
+                            </span><br><br>
+                            Bulletin Null : <strong style="color:red;"> {{ $null }}</strong> voix .....soit <span style="color:green; font-size:1.3em ">
+                             @if ($total >0)
+                             {{round($null  * 100 / $total,2)}} %
+                              @else 0 %
+                            @endif
+                                </span>
                         </p>
                     </div>
                 </div>
